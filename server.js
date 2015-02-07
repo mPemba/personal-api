13e8559cc7aae95b24b78e626f80f978294407d3
Michael Sayer <michael.j.say@gmail.com>
2015-02-07 14:46:49 -0700

    little by little

--- server.js
+++ server.js
@@ -15,7 +15,29 @@ var user = {
 	hobbies: ['music', 'magic'],
 	occupations: ['webDev', 'musician'],
 	mentions: ['https://www.youtube.com/user/PleaseEnjoyTheSpins'],
-	references: ['J-Young', 'JK-Smith', 'Moondog', 'J-Jansen']
+	references: ['J-Young', 'JK-Smith', 'Moondog', 'J-Jansen'],
+	skills: [
+	  {
+	  	id: 1,
+	  	name: 'Song Writing',
+	  	experience: 'intermediate'
+	  },
+	  {
+	  	id: 2,
+	  	name: 'HTML/CSS',
+	  	experience: 'intermediate'
+	  },
+	  {
+	  	id: 3,
+	  	name: 'Music Production', 
+	  	experience: 'intermediate'
+	  },
+	  {
+	  	id: 4,
+	  	name: 'Javascript',
+	  	experience: 'beginner'
+	  }
+	];
 };
 
 app.use(function(req, res, next) {
@@ -34,27 +56,35 @@ app.get('/location', function(req, res) {
 })
 
 app.put('/location', function(req, res) {
-
+    console.log(req.body);
+    if (req.body) {
+    	me.location = req.body.location;
+    	console.log('New Location is: ');
+    	console.log(me.location);
+    	res.status(200).json(me.location);
+    } else {
+    	res.status(400).send('no sir');
+    }
 })
 
 app.get('/hobbies', function(req, res) {
 	if (req.query.reverse === 'desc') {
-		res.status(200).json(hobbies.reverse());
+		res.status(200).json(user.hobbies.reverse());
 	} else if (req.query.alpaOrder === 'asc') {
-		res.status(200).json(hobbies.sort());
+		res.status(200).json(user.hobbies.sort());
 	}
 })
 
 app.get('/occupations', function(req, res) {
 	if (req.query.reverse === 'desc') {
-		res.status(200).json(occupations.reverse());
+		res.status(200).json(user.occupations.reverse());
 	} else if (req.query.alpaOrder === 'asc') {
-		res.status(200).json(occupations.sort());
+		res.status(200).json(user.occupations.sort());
 	}
 })
 
 app.get('/occupations/latest', function(req, res) {
-	res.status(200).json(latestOccupation);
+	res.status(200).json(user.latestOccupation);
 })
 
 app.get('/mentions', function(req, res) {
@@ -62,8 +92,13 @@ app.get('/mentions', function(req, res) {
 })
 
 app.post('/mentions', function(req, res) {
-	console.log(req.body);
-	res.json({message: 'keep it simple stupid'});
+	if (req.body) {
+		console.log('post to mentions: ' + req.body.mention);
+		user.mentions.push(req.body.mentions);
+		res.status(200).json(user.mentions);
+	} else {
+		res.status(400).send('bad bad');
+	}
 })
 
 app.get('/references', function(req, res) {
@@ -71,6 +106,28 @@ app.get('/references', function(req, res) {
 })
 
 app.post('/references', function(req, res) {
-	res.json({message: 'wooooooo'});
+	if (req.body) {
+		console.log('post to references: ' + req.body);
+		user.references.push(req.body);
+		res.status(200).json(me.references);
+	} else {
+		res.status(400).send('bad bad');
+	}
 })
+
+app.get('/skills', function(req, res) {
+	res.status(200).json(user.skills);
+})
+
+app.post('/skills', function(req, res) {
+	if (req.body) {
+		console.log('post to skills: ' + req.body);
+		user.skills.push(req.body);
+		res.status(200).json(user.skills);
+	} else {
+		res.status(400).send('bad bad');
+	}
+}) 
+
+
 
